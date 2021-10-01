663ee1e65e88cd05e847386cad18bd52680b80d9
YoungFalleN <radoss93@mail.ru>
2021-10-02 00:36:32 +0700

    Moved login link methods to BasePage class. Updated locators.

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,12 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class MainPageLocators():
+    pass
 
 
 class LoginPageLocators():
@@ -14,5 +18,7 @@ class ProductPageLocators():
     ADD_TO_CART = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_TITLE = (By.CSS_SELECTOR, ".product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p")
-    TITLE_IN_MESSAGE = (By.CSS_SELECTOR, ".alert-success:first-child .alertinner strong")
+    TITLE_IN_MESSAGE = (By.CSS_SELECTOR,
+                        ".alert-success:first-child .alertinner strong")
     PRICE_IN_MESSAGE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:first-child")
